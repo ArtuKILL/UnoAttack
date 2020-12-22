a500c9b61547f43ac95d8fedcd91d839ee51e8ad
ArtuKILL <artolecuona15@gmail.com>
2020-12-22 10:59:52 -0400

    Primer Commit

---
+++ index.js
@@ -0,0 +1,32 @@
+//Donde se llamarán todos los procesos
+
+
+//Prueba de Electron.js
+
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('./views/index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
